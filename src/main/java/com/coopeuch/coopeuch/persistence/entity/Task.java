75271cbea176e10cf9e44834a4b9c605e4a7515a
bastianilichp <bastianilichp@gmail.com>
2023-05-07 17:59:53 -0400

    SAVE

--- src/main/java/com/coopeuch/coopeuch/persistence/entity/Task.java
+++ src/main/java/com/coopeuch/coopeuch/persistence/entity/Task.java
@@ -8,17 +8,22 @@ import java.util.Date;
 
 @Data
 @Entity
-@Table(name = "task")
+@Table(name = "TASK", schema = "BASTIAN")
+@SequenceGenerator(name = "SEQ_TASK", schema = "BASTIAN", sequenceName = "SEQ_TASK", allocationSize = 1)
 public class Task {
-    @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private Long id;
 
-    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_TASK")
+    @Column(name = "ID", nullable = false)
+    private long id;
+
+    @Column(name = "DESCRIPCION")
     private String descripcion;
-    @Column
+
+    @Column(name = "FECHA_CREACION")
     private Date fechaCreacion;
-    @Column
+
+    @Column(name = "VIGENTE")
     private Boolean vigente;
 
 }
